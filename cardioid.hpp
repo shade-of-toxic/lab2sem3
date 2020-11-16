5edd6b125a4cc315c47f51655e0197b72717eb45
shade-of-toxic <71261437+shade-of-toxic@users.noreply.github.com>
2020-11-16 16:37:23 +0300

    Update cardioid.hpp

--- cardioid.hpp
+++ cardioid.hpp
@@ -1,27 +1,41 @@
 #pragma once
 
+#include <stdexcept>
 #include <cmath>
 #ifndef M_PI
-template <typename T> T const pi = std::acos(-T(1));
+template <typename T>
+T const pi = std::acos(-T(1));
 #else
-template <typename T> T const pi = M_PI;
+template <typename T>
+T const pi = M_PI;
 #endif
 
-struct Cardioid {
+struct Cardioid
+{
 private:
-  double _a;
+double _a;
 
 public:
-  Cardioid(double a = 1) : _a(a) {}
-  double const &getA() const;
-  double &setA(double const &val) { return _a = val; }
-  double getR(double const &angle) const { return 2 * _a * (1 - cos(angle)); }
-  double getMaxLengthFromAxis() const { return 3 * _a; }
-  double getArea() const { return 6 * pi<double> * pow(_a, 2); }
-  double getArcLength(double const &angle) const {
-    return 8 * _a * (1 - cos(angle / 2));
-  }
-  double getRadiusOfCurvative(double const &angle) const {
-    return 8. * _a * sin(angle / 2.) / 3.;
-  }
+Cardioid(double a = 1) {
+if (a < 0)
+throw std::runtime_error("Param 'a' can't be less than zero!");
+}
+double const &getA() const;
+double &setA(double const &val)
+{
+if (val < 0)
+throw std::runtime_error("Param 'a' can't be less than zero!");
+return _a = val;
+}
+double getR(double const &angle) const { return 2 * _a * (1 - cos(angle)); }
+double getMaxLengthFromAxis() const { return 3 * _a; }
+double getArea() const { return 6 * pi<double> * pow(_a, 2); }
+double getArcLength(double const &angle) const
+{
+return 8 * _a * (1 - cos(angle / 2));
+}
+double getRadiusOfCurvative(double const &angle) const
+{
+return 8. * _a * sin(angle / 2.) / 3.;
+}
 };
